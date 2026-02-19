bc6df6376caa8fd7ae2beddf34f2808350f5eb49
Stephen Taylor <stephentaylor@Stephens-Mac-Studio.local>
2026-02-19 10:10:21 -0500

    Adds config and SetUser

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,23 +1,27 @@
 package config
 
 import (
-	"os"
 	"encoding/json"
 	"fmt"
+	"os"
 )
+
 type Config struct {
-	db_url string
-	current_user_name string
+	Db_url            string `json:"db_url"`
+	Current_user_name string `json:"current_user_name"`
 }
 
-func (c *Config) SetUser(userName string) error{
-	c.current_user_name = userName
-	homeDir := os.UserHomeDir()
-	configPath := fmt.Sprintf("%s/.gatorconfig.json")
+func (c *Config) SetUser(userName string) error {
+	c.Current_user_name = userName
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("Error finding home directory: %s", err)
+	}
+	configPath := fmt.Sprintf("%s/.gatorconfig.json", homeDir)
 	jsonData, err := json.Marshal(c)
 	if err != nil {
-		fmt.Printf("Error saving config data: %s", err)
+		return fmt.Errorf("Error saving config data: %s", err)
 	}
-	err := os.WriteFile(configPath, jsonData)
+	err = os.WriteFile(configPath, jsonData, 0644)
 	return nil
 }
